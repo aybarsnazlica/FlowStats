c0168a37760536d300ca831bafcebf3ea45e20c2
Aybars Nazlica <aybarsnazlica@gmail.com>
2026-02-10 01:47:28 +0900

    feat: add a statistics bar chart

--- FlowStats/StatisticsChartView.swift
+++ FlowStats/StatisticsChartView.swift
@@ -5,15 +5,38 @@
 // See LICENSE for license information.
 //
 
-
+import Charts
 import SwiftUI
 
+struct StatisticCount: Identifiable {
+    let id = UUID()
+    let name: String
+    let count: Int
+}
+
 struct StatisticsChartView: View {
+    let data: [StatisticCount]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart {
+            ForEach(data) { statistic in
+                BarMark(
+                    x: .value("Name", statistic.name),
+                    y: .value("Count", statistic.count))
+            }
+        }
+        .chartYAxis {
+            AxisMarks(values: .automatic(desiredCount: 20))
+        }
+        .padding()
     }
 }
 
 #Preview {
-    StatisticsChartView()
+    let data: [StatisticCount] = [
+        .init(name: "Study", count: 2),
+        .init(name: "Projects", count: 3),
+        .init(name: "Work", count: 8)
+    ]
+    StatisticsChartView(data: data)
 }
